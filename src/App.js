5bf647da77e732483239d929eea3b4b00a873e34
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-29 16:18:36 +0500

    Adding time-out animation
    Its a demo of how to apply animation on DOM when its going to hide or delete and toggle a class onto specific element based on some app state.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+import './App.css'
 
 function App() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  let dropdownRef = useRef();
+
+  const openMenu = () => {
+    setToggleMenu(true)
+  }
+
+  const closeMenu = () => {
+    dropdownRef.current.classList.add('slideout')
+    setTimeout(() => (setToggleMenu(false)), 1000)
+  }
+
+  useEffect(() => {
+    if (dropdownRef.current) {
+      let toggleHandler = event => {
+        if (!dropdownRef.current.contains(event.target)) {
+          closeMenu(false)
+        }
+      }
+      document.addEventListener('mousedown', toggleHandler)
+
+      return () => (
+        document.removeEventListener('mousedown', toggleHandler)
+      )
+    }
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dropDownContainer">
+      <button className="searchBtn" onClick={openMenu}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+      {toggleMenu &&
+        <div ref={dropdownRef} id="dropItemsList" className={`dropDownItems`} >
+          <div className="searchBarContainer">
+            <input type="text" name="" placeholder="Search..." id="" />
+            <button onClick={closeMenu}>Search</button>
+          </div>
+        </div>
+      }
     </div>
   );
 }
